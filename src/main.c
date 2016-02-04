e8b573afe2fac28cfad49e1d255e7ea45f9576b7
Ilyes BOUCHLAGHEM <ibouchla@e3r1p5.42.fr>
2016-02-04 22:16:12 +0100

    init commit

--- src/main.c
+++ src/main.c
@@ -6,24 +6,30 @@
 /*   By: ibouchla <ibouchla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 22:59:33 by ibouchla          #+#    #+#             */
-/*   Updated: 2016/02/04 21:17:41 by ibouchla         ###   ########.fr       */
+/*   Updated: 2016/02/04 22:15:42 by ibouchla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_ls.h>
 #include <stdio.h>
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	DIR*	dir;
 	t_dir	*e_dir;
 	t_st	s;
 
-	dir = opendir("."); // Ouvre le dir
-	while ((e_dir = readdir(dir))) // readdir renvoi NULL si erreur ou si end
-		//ft_putendl(e_dir->d_name); // Voir man pour la struct DIR
-	stat(".", &s);
-	printf("Mode : %ho (octal)\n", s.st_mode);
-	closedir(dir);
+	if (ac == 42)
+		return 0;
+	stat(av[1], &s);
+	if (S_ISDIR(s.st_mode))
+	{
+		dir = opendir(av[1]); // Ouvre le dir
+		while ((e_dir = readdir(dir))) // readdir renvoi NULL si erreur ou si end
+			ft_putendl(e_dir->d_name); // Voir man pour la struct DIR
+		closedir(dir);
+	}
+	else if (S_ISREG(s.st_mode))
+		ft_putendl("File detected !");
 	return (0);
 }
