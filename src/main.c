bc78d9d9a73aa23c7108301e3a08bbdc98929b10
Ilyes BOUCHLAGHEM <ibouchla@e2r5p14.42.fr>
2016-02-06 06:18:39 +0100

    init commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ibouchla <ibouchla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 22:59:33 by ibouchla          #+#    #+#             */
-/*   Updated: 2016/02/05 23:30:38 by ibouchla         ###   ########.fr       */
+/*   Updated: 2016/02/06 06:18:01 by ibouchla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,37 @@ static void	ft_init_struct(t_flags *flg)
 	flg->opt_aff = '1';
 }
 
-static void	ft_check_flags(char *av, t_bool error, t_flags *flg)
+static	int	ft_print_error(char *str, char c, int type_error)
+{
+	if (!str || (ft_isprint(c)) == 0)
+		return (-1);
+	if (type_error == 0)
+	{
+		ft_putstr("ft_ls: illegal option -- ");
+		ft_putchar(c);
+		ft_putchar('\n');
+		ft_putendl("usage: ft_ls [-Ralrt1] [file ...]");
+		exit(1);
+	}
+	else if (type_error == 1)
+	{
+		ft_putstr("ft_ls: ");
+		ft_putstr(str);
+		ft_putendl(": No such file or directory");
+	}
+	else if (type_error == 2)
+	{
+		ft_putstr("ft_ls: ");
+		ft_putstr(str);
+		ft_putendl(": File name too long");
+	}
+	return (0);
+}
+
+static void	ft_check_flags(char *av, t_flags *flg)
 {
 	int	id;
 
-	if (error == TRUE)
-	{
-		ft_putendl("ft_ls: No such file or directory");
-		return (1);
-	}
 	id = 0;
 	while (av[++id] != '\0')
 	{
@@ -52,51 +74,52 @@ static void	ft_check_flags(char *av, t_bool error, t_flags *flg)
 			flg->opt_rev = TRUE;
 		else if (av[id] == 't')
 			flg->opt_time = TRUE;
+		else
+			ft_print_error("not recognized", av[id], 0);
 	}
 }
-static	void	ft_print_error(void)
-{
-	ft_putendl("ft_ls: illegal option -- -");
-	ft_putendl("usage: ft_ls [-Ralrt1] [file ...]");
-	exit(1);
-}
+
 int	main(int ac, char **av)
 {
 	t_flags	flg;
 	t_env	*e;
-/*
 	t_dir	*elem_dir;
 	t_st	s;
-*/
+
 	ft_init_struct(&flg);
 	e = ft_memalloc(sizeof(e));
 	while (++e->x < ac)
 	{
 		if (av[e->x][0] == '-')
 		{
-			if (av[e->x][1] == '-' && e->error == TRUE)
-				ft_putstr("ft_ls: --x No such file or directory");
-			else if (av[e->x][1] == '-' && e->error == FALSE)
+			if (av[e->x][1] == '-' && e->error == FALSE)
 			{
-				if (av[e->x][2] est ascii)
-					ft_print_error();
+				if ((ft_isprint(av[e->x][2])) == 1)
+					ft_print_error(av[e->x], '-', 0);
 				e->error = TRUE;
 			}
-			ft_check_flags(av[e->x], e->error, &flg);
+			else if (e->error == TRUE)
+				ft_print_error(av[e->x], ' ', 1);
+			else
+				ft_check_flags(av[e->x], &flg);
 		}
-		else
+		else if ((ft_strlen(av[e->x])) > 255)
+			ft_print_error(av[e->x], ' ', 2);
+		if (av[e->x][0] != '-')
 		{
-			stat(av[x], &s);
+			stat(av[e->x], &s);
+			//ft_list_type();
 			if (S_ISDIR(s.st_mode))
 			{
-				if (!(e.dir = opendir(av[x]))) // Ouvre le dir
+				if (!(e->dir = opendir(av[e->x]))) // Ouvre le dir
 				{
-					perror("ft_ls");
+					ft_putstr("ft_ls: ");
+					perror(elem_dir->d_name);
 					exit(1);
 				}
-				while ((elem_dir = readdir(e.dir))) // readdir renvoi NULL si erreur ou si end
+				while ((elem_dir = readdir(e->dir))) // readdir renvoi NULL si erreur ou si end
 					ft_putendl(elem_dir->d_name); // Voir man pour la struct DIR
-				if ((closedir(e.dir)) == (-1))
+				if ((closedir(e->dir)) == (-1))
 					exit(-1);
 			}
 			else if (S_ISREG(s.st_mode))
